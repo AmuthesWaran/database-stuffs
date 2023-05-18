0ba02dd61abefb97bfd8e284256963075bbcd16b
Amutheswaran <amutheswaran@outlook.com>
2023-05-18 05:40:51 +0530

    additial statements added

--- Oracle/Oracle PL SQL/00_SQL_Statements.sql
+++ Oracle/Oracle PL SQL/00_SQL_Statements.sql
@@ -1,7 +1,12 @@
 --ALL_TABLES describes the relational tables accessible to the current user
 SELECT * FROM all_tables;
 SELECT DISTINCT(owner) FROM all_tables;
+SELECT * FROM all_tables where owner='HR';
+
+-- Accessing Tables
+SELECT * FROM HR.employees;
 
 --ALL_TAB_COLUMNS describes the columns of the tables, views, and clusters accessible to the current user.
 SELECT * FROM ALL_TAB_COLUMNS;
 SELECT * FROM ALL_TAB_COLUMNS WHERE owner ='HR';
+
